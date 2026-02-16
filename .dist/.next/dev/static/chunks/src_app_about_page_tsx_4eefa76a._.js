d3abdfe23dd720965c5b45b17e1cba03fb7e7ec6
KK._.097 <203602800+kartik098ki@users.noreply.github.com>
2026-02-16 09:10:02 +0530

    hey

--- .dist/.next/dev/static/chunks/src_app_about_page_tsx_4eefa76a._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_d81cd595._.js",
-  "static/chunks/node_modules_57f8c6af._.js"
-],
-    source: "dynamic"
-});
